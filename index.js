19b38f9837891c91a1fae37b925fbff91004b6e0
Siddharth Tiwari <tsiddharth838@gmail.com>
2026-02-07 22:45:36 +0530

    Fix: defined __dirname for ES modules and added navigation styling

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'; 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+const publicPath = path.join(__dirname, 'public');
 
+app.use(express.static(publicPath));
 app.set("view engine",'ejs')
+
+
 app.get("/",(req,resp)=>{
     resp.render("list")
 })
